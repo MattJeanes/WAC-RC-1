b4a53b74ac2eed4b5b2461df7dc4abb0df913dca
Matt Jeanes <mattjeanes23@gmail.com>
2013-05-21 21:33:37 +0100

    Alpha
    A small c-130 is included in this alpha release, don't expect it to work
    brilliantly yet.

---
+++ lua/entities/wac_pl_c130_rc/cl_init.lua
@@ -0,0 +1,122 @@
+
+include("shared.lua")
+
+ENT.thirdPerson = {
+	distance = 1200
+}
+
+local HudMat = Material("WeltEnSTurm/helihud/arrow")
+local HudCol = Color(70,199,50,150)
+local Black = Color(0,0,0,200)
+function ENT:DrawPilotHud()
+	local pos = self:GetPos()
+	local fwd = self:GetForward()
+	local ri = self:GetRight()
+	local ang = self:GetAngles()
+	ang:RotateAroundAxis(ri, 90)
+	ang:RotateAroundAxis(fwd, 90)
+
+	local uptm = self.rotorRpm
+	local upm = self.SmoothUp
+	cam.Start3D2D(self:LocalToWorld((Vector(20,3.75,37.75)*self.Scale)+self.Seats[1].pos), ang,0.015*self.Scale)
+	surface.SetDrawColor(HudCol)
+	surface.DrawRect(235, 249, 10, 2)
+	surface.DrawRect(255, 249, 10, 2)
+	surface.DrawRect(249, 235, 2, 10)
+	surface.DrawRect(249, 255, 2, 10)
+	surface.DrawRect(-3, 0, 3, 500)
+	surface.DrawRect(500, 0, 3, 500)
+	surface.DrawRect(7, 0, 3, 500)
+	surface.DrawRect(490, 0, 3, 500)
+
+	surface.DrawRect(-6,-3,19,3)
+	surface.DrawRect(-6,500,19,3)
+	surface.DrawRect(487,-3,19,3)
+	surface.DrawRect(487,500,19,3)
+	surface.DrawRect(9,248,5,3)
+	surface.DrawRect(485,248,5,3)
+
+	surface.DrawRect(1, 500-uptm*500, 5, uptm*500)
+	surface.DrawLine(30, 5*ang.r-200+2.77*ang.p, 220, 5*ang.r-200+2.77*(ang.p*0.12))
+	surface.DrawLine(30, 5*ang.r-200+2.77*ang.p+1, 220, 5*ang.r-200+2.77*(ang.p*0.12)+1)
+	surface.DrawLine(280, 5*ang.r-200-2.77*(ang.p*0.12), 470, 5*ang.r-200-2.77*ang.p)
+	surface.DrawLine(280, 5*ang.r-200-2.77*(ang.p*0.12)+1, 470, 5*ang.r-200-2.77*ang.p+1)
+	surface.SetMaterial(HudMat)
+	surface.DrawTexturedRect(-20,250-upm*250-10,20,20)
+	surface.DrawTexturedRectRotated(498,math.Clamp(250-self:GetVelocity().z/5.249*2,0,500),20,20,180)
+	surface.SetTextColor(HudCol)
+	surface.SetFont("wac_heli_small")
+	surface.SetTextPos(-10, 505) 
+	surface.DrawText("SPD")
+	surface.SetTextPos(-10, 520)
+	surface.DrawText(math.floor(self:GetVelocity():Length()*0.1)) --knots (real would be 0.037147, but fuck it)
+
+	if self:GetHover() then
+		surface.SetTextColor(HudCol)
+		surface.SetFont("wac_heli_small")
+		surface.SetTextPos(483, -18)
+		surface.DrawText("HVR")
+	end
+
+	local tr=util.QuickTrace(pos,Vector(0,0,-999999),self.Entity)
+	surface.SetTextPos(485,505)
+	surface.DrawText("ALT")
+	surface.SetTextPos(485,520)
+	surface.DrawText(math.floor((pos.z-tr.HitPos.z)/16))
+
+	cam.End3D2D()
+end
+
+function ENT:DrawWeaponSelection()
+	local fwd = self:GetForward()
+	local ri = self:GetRight()
+	local ang = self:GetAngles()
+	ang:RotateAroundAxis(ri, 90)
+	ang:RotateAroundAxis(fwd, 90)
+	for k,t in pairs(self.Seats) do
+		if k != "BaseClass" and !t.NoHud then
+			cam.Start3D2D(self:LocalToWorld((Vector(20,5,25)*self.Scale)+t.pos), ang, 0.02*self.Scale)
+			surface.DrawRect(-10, 0, 500, 30)
+			surface.DrawRect(-10, 30, 10, 20)
+			if t.weapons then
+				local active = self:GetNWInt("seat_"..k.."_actwep")
+				if t.weapons[active] then
+					local weapon = self.weapons[t.weapons[active]]
+					local lastshot = weapon:GetLastShot()
+					local nextshot = weapon:GetNextShot()
+					local ammo = weapon:GetAmmo()
+					surface.DrawRect(10, 40, math.Clamp((nextshot-CurTime())/(nextshot-lastshot), 0, 1)*480, 10)
+					draw.SimpleText(k.." "..t.weapons[active], "wac_heli_big", 0, -2.5, Black, 0)
+					draw.SimpleText(ammo, "wac_heli_big", 480, -2.5, Black, 2)
+				end
+			else
+				draw.SimpleText(k, "wac_heli_big", 0, -2.5, Black, 0)
+			end
+			cam.End3D2D()
+		end
+	end
+end
+
+function ENT:viewCalcFirstPerson(k, p, view)
+	p.wac = p.wac or {}
+	local vec=Vector(2.5,0,26)
+	if
+		k == 1
+		and p:GetInfo("wac_cl_air_mouse") == "1"
+		and !p.wac.viewFree
+	then
+		self.viewTarget = {
+			origin = -vec,
+			angles = Angle(0,0,0),
+			fov = view.fov
+		}
+	else
+		self.viewTarget = {
+			origin = -vec,
+			angles = p:GetAimVector():Angle() - self:GetAngles(),
+			fov = view.fov
+		}
+		self.viewTarget.angles.r = self.viewTarget.angles.r + self:GetAngles().r
+	end
+	return view
+end
---
+++ lua/entities/wac_pl_c130_rc/shared.lua
@@ -0,0 +1,97 @@
+if not wac then return end
+ENT.Base = "wac_pl_base"
+ENT.Type = "anim"
+ENT.Category = wac.aircraft.spawnCategoryRC
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+ENT.PrintName = "C-130J Hercules"
+
+ENT.Model				= "models/drmatt/c130_rc/body.mdl"
+ENT.RotorPhModel		= "models/props_junk/sawblade001a.mdl"
+ENT.RotorModel			= "models/drmatt/c130_rc/propellor.mdl"
+
+ENT.FirePos			= Vector(133,-228,160)
+ENT.SmokePos		= ENT.FirePos
+
+ENT.Weight			= 8750
+ENT.EngineForce		= 55
+ENT.rotorPos 	= Vector(35,-114.38,49.41)
+ENT.Scale = 0.25
+
+ENT.OtherRotorPos={
+	Vector(35,-57.25,47),
+	Vector(35,56.08,47),
+	Vector(35,113.02,49.41)
+}
+
+ENT.Wheels={
+	{
+		mdl="models/drmatt/c130_rc/wheel.mdl",
+		pos=Vector(-20,26,6),
+		friction=0,
+		mass=375,
+	},
+	{
+		mdl="models/drmatt/c130_rc/wheel.mdl",
+		pos=Vector(-3,26,6),
+		friction=0,
+		mass=375,
+	},
+	{
+		mdl="models/drmatt/c130_rc/wheel.mdl",
+		pos=Vector(-20,-27,6),
+		friction=0,
+		mass=375,
+	},
+	{
+		mdl="models/drmatt/c130_rc/wheel.mdl",
+		pos=Vector(-3,-27,6),
+		friction=0,
+		mass=375,
+	},
+	{
+		mdl="models/drmatt/c130_rc/wheel.mdl",
+		pos=Vector(99,-3.5,6),
+		friction=0,
+		mass=375,
+	},
+	{
+		mdl="models/drmatt/c130_rc/wheel.mdl",
+		pos=Vector(99,2.5,6),
+		friction=0,
+		mass=375,
+	},
+}
+
+ENT.Seats = {
+	{
+		pos=Vector(106.43,5.3,33),
+		exit=Vector(103.47,61.72,5),
+	},
+	{
+		pos=Vector(420,-28.94,125),
+		exit=Vector(425.42,140,125),
+	},
+	{
+		pos=Vector(371.79,-40,119.66),
+		ang=Angle(0,-90,0),
+		exit=Vector(425.42,140,125),
+	},
+	{
+		pos=Vector(340,-18.87,119.66),
+		exit=Vector(425.42,140,125),
+	},
+}
+
+ENT.Sounds={
+	Start="WAC/c130/startup.wav",
+	Blades="^WAC/c130/external.wav",
+	Engine="WAC/c130/internal.wav",
+	MissileAlert="HelicopterVehicle/MissileNearby.mp3",
+	MissileShoot="HelicopterVehicle/MissileShoot.mp3",
+	MinorAlarm="HelicopterVehicle/MinorAlarm.mp3",
+	LowHealth="HelicopterVehicle/LowHealth.mp3",
+	CrashAlarm="HelicopterVehicle/CrashAlarm.mp3"
+}
+
+function ENT:DrawWeaponSelection() end
